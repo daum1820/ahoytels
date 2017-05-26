93b49e2ac743265236c953b4803860877a9e40c2
Felipe Herrerias <daum1820@gmail.com>
2017-05-26 01:17:31 -0300

    Javascript Setup - Base URL

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+import webpack, { ProvidePlugin } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
     template: `${__dirname}/src/index.html`,
@@ -9,25 +9,33 @@ const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
     inject: 'body',
 });
 
+const DefinePluginConfig = new webpack.DefinePlugin({   
+    'process.env.BASE_URL': JSON.stringify('http://fake-hotel-api.herokuapp.com'),
+});
+
+const ExtractTextPluginConfig = new ExtractTextPlugin({ 
+    filename: 'bundle.css', allChunks: true 
+});
+
+const WebpackProvidePluginConfig = new ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+});
+
 const plugins = [
     HTMLWebpackPluginConfig,
-        new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-    }),
-    new ExtractTextPlugin({ filename: 'bundle.css', disable: false, allChunks: true }),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.ContextReplacementPlugin(/moment[\\\/]locale$/, /^\.\/(en)$/), // To load only en|pt-br in momentjs
-    new webpack.optimize.UglifyJsPlugin({
-        compress: { warnings: false },
-        comments: false,
-        sourceMap: true,
-        minimize: false,
-    }),
+    DefinePluginConfig,
+    ExtractTextPluginConfig,
+    WebpackProvidePluginConfig
 ];
 
 const loaders = [
-    { test: /.js?$/, loader: 'babel-loader', include: [path.resolve('src')] },
+    {
+        test: /.jsx?$/,
+        loader: 'babel-loader', include: [path.resolve('src')], 
+        include: [path.resolve('src')],
+        exclude: [/node_modules/],
+    },
     {
         test: /\.s?css$/,
         loaders: ExtractTextPlugin.extract({
@@ -35,34 +43,34 @@ const loaders = [
             use: 'css-loader!sass-loader',
         }),
     },
-    { test: /\.json$/, loader: 'json-loader' },
-    { test: /\.html$/, loader: 'html-loader', query: { minimize: true } },
+    { test: /\.html$/, loader: 'html-loader' },
     { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
     { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
     { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'file-loader' },
-    { test: /\.(mp3)$/i, loader: 'file-loader' },
 ];
 
-module.exports = {
+const webpackConfig = {
     entry: './src/index.js',
     output: { path: `${__dirname}/dist`, filename: 'build.js' },
-    devtool: 'source-map',
+    cache: true,
+    devtool: 'inline-eval-cheap-source-map',
     devServer: {
         clientLogLevel: 'info',
         historyApiFallback: true,
         inline: true,
     },
+    watch: true,
     resolve: {
-        extensions: [
-            '.js',
-        ],
+        extensions: ['.js'],
         modules: [
             path.resolve('./src'),
             path.resolve('./node_modules'),
-        ]
+        ],
     },
     module: {
         rules: loaders,
     },
     plugins,
 };
+
+export default webpackConfig;
